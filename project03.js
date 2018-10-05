7c5d65f7f7f9c931560d4c579a84810508ea3b27
leemeg <marshalll@student.ncmich.edu>
2018-10-05 08:57:08 -0400

    save before "for" testing

--- project03.js
+++ project03.js
@@ -11,41 +11,37 @@ const PROMPT = require('readline-sync');
 
 let wrongNum = 0; // presets incorrect response attempts to max number of tries
 let numVotes = 0; //resets votes for new run
-let rerun = 1, stars, result;
+let stars, result;
 let movieTitle = "Howard Bates is awesome";
 let arrayStars = [];
 
 function main() {
     //setReRun();
-    while (rerun === 1) {
+    while (stars !== 99) {
         setStars();
-        populateArrayStars();
+
         //setReRun();
     }
     setResults();
 }
 main();
 
-function setReRun() {
-    if (rerun === 1 || rerun === 0) {
-        rerun = Number(PROMPT.question(`\nWould you like to rate a movie? [0=no, 1=yes]: `));
-        while (rerun !== 0 && rerun !== 1) {
-            console.log(`${rerun} is an incorrect value. Please try again.`);
-            rerun = Number(PROMPT.question(`\nDo you want to continue? [0=no, 1=yes]: `));
-        }
-    }else {
-        rerun = 1;
-    }
-}
+
 
 function setStars() {
     const NUMTRY = 3;
+    const END = 99;
+    //process.stdout.write('\x1Bc');
     console.log(`\nWith 0 stars being worst and 5 stars equaling the best, how would you rate the movie "${movieTitle}" ?`);
+    console.log(`Or if you no longer wish to participate, enter 99.`);
     stars = Number(PROMPT.question(`Please enter a number from 0 to 5 representing how many stars you would give this movie: `));
-    if (stars === 99){
-        rerun = 0;
+
+    if ((stars >= 0 && stars <= 5) && stars !== END) {
+        wrongNum = 0;
+        (arrayStars.push(stars));
+        numVotes++;
     }
-    else if (stars < 0 || stars > 5) {
+    else if ((stars < 0 || stars > 5) && (stars !== END)){
         wrongNum++;
         if (wrongNum < (NUMTRY - 1)) {
             console.log(`\nIncorrect rating, please try again.`);
@@ -60,22 +56,15 @@ function setStars() {
             stars = null;
         }
     }
-    wrongNum = 0;
+
 }
 
-function populateArrayStars() {
-    if (stars === 99) {
 
-    }
-    else if (arrayStars.push(stars)) {
-        numVotes++;
-    }
-}
 
 function setResults() {
     let sum;
     process.stdout.write('\x1Bc');
-    sum = arrayStars.reduce(function (a, b) {return a + b;}, 0);
+    sum = arrayStars.reduce(function (a, b) {return a + b;}, 0);//https://stackoverflow.com/questions/1230233/how-to-find-the-sum-of-an-array-of-numbers
     result = (sum / numVotes);
     console.log(`\nWith ` + numVotes + ` votes, the average star rating of the movie "` + movieTitle + `" is ` + result + ` stars.`);
 }
